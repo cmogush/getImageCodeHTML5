0525db30c8d0a82361b17c3eecc3bd2cc696ac86
cmogush <cmogush@gmail.com>
2020-04-20 14:48:21 -0400

    Modified getImageCode to be an html file
    Now displays as a table with the image on the left and the code on the right

--- getImageCode.py
+++ getImageCode.py
@@ -3,7 +3,10 @@ from PIL import Image
 from pathlib import Path
 
 def getImageTag(repo, image, height, width):
-    return "<img src=\""+repo+image+"\" height=\""+height+"\" width=\""+width+"\" alt=\"\" />"
+    return "&lt;img src=\""+repo+image+"\" height=\""+height+"\" width=\""+width+"\" alt=\"\" /&gt;"
+
+def getLocalTag(dir, image, height, width):
+    return "<img src=\""+ dir + "\\" + image + "\" height=\"" + height + "\" width=\"" + width + "\" alt=\"\" align=\"right\"/>"
 
 def writeImageCode(parent, currDir, file, repo):
     dir = parent+"\\"+currDir
@@ -16,25 +19,29 @@ def writeImageCode(parent, currDir, file, repo):
             continue
         img = Image.open(dir + "/" + f)
         width, height = img.size
-        file.write(getImageTag(repo, f, str(height), str(width)) + "\n")
+        imgTag = getImageTag(repo, f, str(height), str(width))
+        localTag = getLocalTag(dir, f, str(height), str(width))
+        file.write("<tr><td>"+localTag+"</td><td>\""+str(imgTag)+"\"</td></tr>")
 
 def main():
     print("Enter eSchoolware Repository (without quotes), e.g:")
     print("/suite/repository/workspace/courses/4275/12316/")
-    repo = input("Input: ")
-    # repo = r"/suite/repository/workspace/courses/4275/12316/"
+    # repo = input("Input: ")
+    repo = r"/suite/repository/workspace/courses/4275/12316/"
 
     print("Enter local image directory to scan: ")
-    # cwd = Path(r"D:\Edison\_NewDevelopment\2020\Math PSSA Prep\CE\images\assessment_pool_images")
-    cwd = Path(input("Input: "))
+    # cwd = Path(input("Input: "))
+    cwd = Path(r"D:\Edison\_NewDevelopment\2020\Math PSSA Prep\CE\images\assessment_pool_images")
 
     parent = str(cwd.parent)
     dir = str(os.path.basename(cwd))
-    imgCode = parent+"\\"+dir+"\html_image_code.txt"
+    imgCode = parent+"\\"+dir+"\html_image_code.html"
 
     """write the code to the file"""
     with open(imgCode, "w+") as file:
+        file.write("<html>\n<body>\n<table>\n")
         writeImageCode(parent, dir, file, repo)
+        file.write("</table>\n</body>\n</html>")
 
 if __name__ == "__main__":
     main()
